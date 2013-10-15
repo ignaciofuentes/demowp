d364dde9e97a1b874c4618de72854f1253323073
Ignacio Fuentes <ignaciofuentes@outlook.com>
2013-10-15 17:05:30 -0400

    app

---
+++ StackOJobs/StackOJobs/ViewModels/Job.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+
+namespace StackOJobs.ViewModels
+{
+    public class Job : INotifyPropertyChanged
+    {
+        private string _id;
+        /// <summary>
+        /// Sample ViewModel property; this property is used to identify the object.
+        /// </summary>
+        /// <returns></returns>
+        public string ID
+        {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                if (value != _id)
+                {
+                    _id = value;
+                    NotifyPropertyChanged("ID");
+                }
+            }
+        }
+        private bool _important;
+        public bool Important
+        {
+            get
+            {
+                return _important;
+            }
+            set
+            {
+                if (value != _important)
+                {
+                    _important = value;
+                    NotifyPropertyChanged("Important");
+                }
+            }
+        }
+
+        private string _name;
+        /// <summary>
+        /// Sample ViewModel property; this property is used in the view to display its value using a Binding.
+        /// </summary>
+        /// <returns></returns>
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (value != _name)
+                {
+                    _name = value;
+                    NotifyPropertyChanged("Name");
+                }
+            }
+        }
+
+        private string _companyName;
+        /// <summary>
+        /// Sample ViewModel property; this property is used in the view to display its value using a Binding.
+        /// </summary>
+        /// <returns></returns>
+        public string CompanyName
+        {
+            get
+            {
+                return _companyName;
+            }
+            set
+            {
+                if (value != _companyName)
+                {
+                    _companyName = value;
+                    NotifyPropertyChanged("CompanyName");
+                }
+            }
+        }
+
+        private string _description;
+        /// <summary>
+        /// Sample ViewModel property; this property is used in the view to display its value using a Binding.
+        /// </summary>
+        /// <returns></returns>
+        public string Description
+        {
+            get
+            {
+                return _description;
+            }
+            set
+            {
+                if (value != _description)
+                {
+                    _description = value;
+                    NotifyPropertyChanged("Description");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (null != handler)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+
+    }
+}
---
+++ StackOJobs/StackOJobs/ViewModels/MainViewModel.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using StackOJobs.Resources;
+using System.Windows;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Xml.Linq;
+using System.Linq;
+
+namespace StackOJobs.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        HttpClient client;
+        public MainViewModel()
+        {
+            client = new HttpClient();
+            this.Items = new ObservableCollection<Job>();
+        }
+
+        /// <summary>
+        /// A collection for ItemViewModel objects.
+        /// </summary>
+        public ObservableCollection<Job> Items { get; private set; }
+
+
+        /// <summary>
+        /// Sample property that returns a localized string
+        /// </summary>
+
+        public bool IsDataLoaded
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Creates and adds a few ItemViewModel objects into the Items collection.
+        /// </summary>
+        public async Task LoadData()
+        {
+            // Sample data; replace with real data            
+            try
+            {
+                var response = await client.GetAsync("http://careers.stackoverflow.com/jobs/feed");
+                var content = await response.Content.ReadAsStringAsync();
+                var xml = XElement.Parse(content);
+                var channel = xml.Elements("channel");
+                foreach (var item in channel.Elements("item"))
+                {
+                    var x = (string)item.Element("title");
+                    var des = (string)item.Element("description");
+                    var p = x.IndexOf("at");
+
+                    var name = x.Substring(0, p);
+                    var company = x.Substring(p += 2, x.Length - p);
+                    var j = new Job { ID=(string)item.Element("guid"), Name = name.Trim(), CompanyName = company.Trim(), Description=des };
+                    this.Items.Add(j);
+                    IsDataLoaded = true;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Check your internet connection and try again", "Something bad happened", MessageBoxButton.OK);
+            }
+
+            //this.Items.Add(new Job() { ID = "0", Name = "runtime one", CompanyName = "Maecenas praesent accumsan bibendum", LineThree = "Facilisi faucibus habitant inceptos interdum lobortis nascetur pharetra placerat pulvinar sagittis senectus sociosqu" });
+
+            // return x;
+
+            //this.IsDataLoaded = true;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (null != handler)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
